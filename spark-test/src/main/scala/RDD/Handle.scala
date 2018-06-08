f37636a1108f4f85ff37df05a9c3e8acaf8365a8
youtna <youtna16@gmail.com>
2018-06-09 00:05:19 +0800

    add set RDD handle

--- spark-test/src/main/scala/RDD/Handle.scala
+++ spark-test/src/main/scala/RDD/Handle.scala
@@ -32,6 +32,42 @@ object Handle {
     printOutPut(flatMapRDD)
     println("flatMap 和map的区别，我们可以flatMap看做把map返回值的迭代器\"拍扁\"")
     printOutPut(mapRDD2)
+
+    /**
+      * distinct test
+      */
+    val distinctRDD = text.distinct().collect()
+    println("对RDD元素去重")
+    printOutPut(distinctRDD)
+
+    /**
+      * union test
+      */
+    val text2 = sc.textFile("RDDFILE")
+    val unionRDD = text.union(text2).collect()
+    println("生成一个包含两个RDD中所有元素的RDD")
+    printOutPut(unionRDD)
+
+    /**
+      * intersection test
+      */
+    val intersectionRDD = text.intersection(text2).collect()
+    println("只返回两个RDD都有的元素")
+    printOutPut(intersectionRDD)
+
+    /**
+      * subtract test
+      */
+    val subtractRDD = text.subtract(text2).collect()
+    println("subtract 接收另一个RDD作为参数，返回一个有只存在第一个RDD中而不存在第二个RDD中的所有元素组成的RDD")
+    printOutPut(subtractRDD)
+
+    /**
+      * cartesian test
+      */
+    val cartesianRDD = text.cartesian(text2).collect()
+    println("笛卡尔积")
+    printOutPut(cartesianRDD)
   }
 
   def printOutPut[T](rDDs: Array[T]): Unit = {
