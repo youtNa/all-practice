d56594baaa3b953a0b747285660533e9115a04bb
youtNa <youtna16@gmail.com>
2019-01-24 18:36:50 +0800

    modify streaming api

--- spark-test/src/main/scala/streaming/Api.scala
+++ spark-test/src/main/scala/streaming/Api.scala
@@ -1,6 +1,5 @@
 package streaming
 
-import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -12,23 +11,16 @@ import scala.collection.mutable
   */
 object Api {
   def main(args: Array[String]): Unit = {
-    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
-    if (!log4jInitialized) {
-      // We first log something to initialize Spark's default logging, then we override the
-      // logging level.
-      //      logInfo("Setting log level to [WARN] for streaming example." +
-      //        " To override add a custom log4j.properties to the classpath.")
-      Logger.getRootLogger.setLevel(Level.WARN)
-    }
     val sparkConf = new SparkConf().setAppName("api").setMaster("local[2]")
     val rddQueue = new mutable.Queue[RDD[Int]]()
+
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     val lines = ssc.queueStream(rddQueue)
-    val d = lines.map(_ + 1)
-
+    val d = lines.map(_ + 1).filter(_ > 4)
     d.print()
-    ssc.start()
 
+
+    ssc.start()
 
     for (i <- 1 to 30) {
       rddQueue.synchronized {
